aaef64f089641a001cd72367e74de9a2af93eb40
Kajal Singh <99634562+kajal-singh5@users.noreply.github.com>
2023-07-10 20:43:09 +0530

    added loading animation

---
+++ scripts/script.js
@@ -0,0 +1,26 @@
+// Moving gradient for hero section
+const gradientBorder = document.querySelector('.hero-wrapper');
+let angle = 55;
+
+function incrementAngle() {
+    angle += 2;
+    angle %= 360;
+    gradientBorder.style.setProperty('--angle', `${angle}deg`);
+}
+
+setInterval(incrementAngle, 45);
+
+
+// Move loading screen up
+setTimeout(() => {
+    const div1 = document.querySelector('.loading');
+    div1.classList.add('slide-up');
+}, 1000);
+
+setTimeout(() => {
+    const div1 = document.querySelector('.loading');
+    div1.classList.add('slide-up');
+
+    const div2 = document.querySelector('.main-content');
+    div2.style.display = 'block';
+}, 6000);
