4e4be33807b73bd726bc8e7b5254a8fd47c8d3c0
Kajal Singh <99634562+kajal-singh5@users.noreply.github.com>
2023-07-15 01:40:58 +0530

    added responsive rules

--- scripts/script.js
+++ scripts/script.js
@@ -27,39 +27,67 @@ setTimeout(() => {
 
 
 // MAP
+const mediaQuery = window.matchMedia("(max-width: 798px)");
 const openModalButton = document.querySelector('.open-modal');
 const closeModalButton = document.querySelector('.close-modal');
 const modal = document.querySelector('.modal');
 const modalContent = document.querySelector('.modal-content');
 const modalGifOpening = document.querySelector('.modal-gif.opening');
 const modalGifClosing = document.querySelector('.modal-gif.closing');
+const internalLinks = document.querySelector('.internal-links');
+const internalLinkItems = internalLinks.children;
+const externalLinks = document.querySelector('.external-links');
 
-openModalButton.addEventListener('click', function() {
-  modalGifOpening.src = './assets/nav/open-modal4.gif';
-  modal.style.display = 'flex';
-  document.body.style.overflow = 'hidden';
-  modalGifOpening.style.opacity = '0.95';
+if (mediaQuery.matches) {
+    openModalButton.addEventListener('click', function() {
+        modal.style.display = 'flex';
+        document.body.style.overflow = 'hidden';
+        modalContent.style.opacity = '1';
+        modalContent.style.display = 'flex';
+        internalLinks.style.flexDirection = 'column';
+        internalLinks.style.alignItems = 'center';
+        for (var i = 0; i < internalLinkItems.length; i++) {
+            var child = internalLinkItems[i];
+            child.style.alignSelf = 'auto';
+        }
+    });
 
-  setTimeout(function() {
-    modalContent.style.opacity = '1';
-  }, 1000);
+    closeModalButton.addEventListener('click', function() {
+        modalContent.style.opacity = '0';
+        modalContent.style.display = 'none';
+        modal.style.display = 'none';
+        document.body.style.overflow = 'auto';
+    });
+} else {
+    openModalButton.addEventListener('click', function() {
+        modalGifOpening.src = './assets/nav/open-modal4.gif';
+        modal.style.display = 'flex';
+        document.body.style.overflow = 'hidden';
+        modalGifOpening.style.opacity = '0.95';
+        
+        setTimeout(function() {
+            modalContent.style.opacity = '1';
+        }, 1000);
+        
+    });
 
-});
+    closeModalButton.addEventListener('click', function() {
+        modalGifClosing.src = './assets/nav/close-modal4.gif';
+        modalGifOpening.style.opacity = '0';
+        modalGifClosing.style.opacity = '0.95';
+        
+        setTimeout(function() {
+          modalContent.style.opacity = '0';
+        }, 200);
+      
+        setTimeout(function() {
+          modalGifClosing.style.opacity = '0';
+          modalGifOpening.src = './assets/nav/open-modal4.gif';
+          modal.style.display = 'none';
+          document.body.style.overflow = 'auto';
+        }, 1500);
+      
+    });
+}
 
-closeModalButton.addEventListener('click', function() {
-  modalGifClosing.src = './assets/nav/close-modal4.gif';
-  modalGifOpening.style.opacity = '0';
-  modalGifClosing.style.opacity = '0.95';
-  
-  setTimeout(function() {
-    modalContent.style.opacity = '0';
-  }, 200);
 
-  setTimeout(function() {
-    modalGifClosing.style.opacity = '0';
-    modalGifOpening.src = './assets/nav/open-modal4.gif';
-    modal.style.display = 'none';
-    document.body.style.overflow = 'auto';
-  }, 1500);
-
-});
